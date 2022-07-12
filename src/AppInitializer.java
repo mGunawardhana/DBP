2ecf8081af86912287cfb9eba1ada8e669a81c4a
mGunawardhana <mrgunawardhana27368@gmail.com>
2022-07-12 08:58:48 +0530

    DBConnection setup

---
+++ src/AppInitializer.java
@@ -0,0 +1,20 @@
+/*
+ *  * Developed by - mGunawardhana
+ *  * Contact email - mrgunawardhana27368@gmail.com
+ *  * what's app - 071 - 9043372
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
---
+++ src/db/DBConnection.java
@@ -0,0 +1,29 @@
+/*
+ *  * Developed by - mGunawardhana
+ *  * Contact email - mrgunawardhana27368@gmail.com
+ *  * what's app - 071 - 9043372
+ */
+
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static DBConnection dbConnection = null;
+    private final Connection connection;
+
+    private DBConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/  ", "root", "1234");
+    }
+
+    public static DBConnection getInstance() throws SQLException, ClassNotFoundException {
+        return (dbConnection == null) ? dbConnection = new DBConnection() : dbConnection;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
